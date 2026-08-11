e71a9c0c61bd180b7a59351cd9d573069159ff5c
send0xx <ionkin.misha@gmail.com>
2026-08-11 23:33:00 +0300

    remove useles tests

--- tests/Dispatcher.Tests/Registry/DispatcherRegistryTests.cs
+++ tests/Dispatcher.Tests/Registry/DispatcherRegistryTests.cs
@@ -1,37 +1,10 @@
-using System.Collections.Frozen;
-using System.Reflection;
 using Dispatcher.Tests.TestSupport;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Dispatcher.Tests.Registry;
 
 public sealed class DispatcherRegistryTests
 {
-    [Fact]
-    public void Reflection_runtime_types_share_the_dependency_injection_assembly()
-    {
-        Assert.Same(typeof(global::Dispatcher.Dispatcher).Assembly, typeof(DispatcherRegistry).Assembly);
-        Assert.NotSame(typeof(HandlerRegistration).Assembly, typeof(DispatcherRegistry).Assembly);
-    }
-
-    [Fact]
-    public void Handler_registrations_contain_metadata_only()
-    {
-        Type[] registrationTypes =
-        [
-            typeof(QueryHandlerRegistration),
-            typeof(CommandWithResponseHandlerRegistration),
-            typeof(CommandHandlerRegistration),
-            typeof(NotificationHandlerRegistration)
-        ];
-
-        Assert.All(registrationTypes, registrationType =>
-            Assert.DoesNotContain(
-                registrationType.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic),
-                property => property.Name.Contains("Wrapper", StringComparison.Ordinal)));
-    }
-
     [Fact]
     public void Duplicate_request_handlers_are_rejected_when_registry_is_built()
     {
@@ -42,16 +15,5 @@ public sealed class DispatcherRegistryTests
         };
 
         Assert.Throws<DuplicateHandlerException>(() => DispatcherRegistry.Create(registrations));
-    }
-
-    [Fact]
-    public async Task Registry_uses_frozen_dictionaries()
-    {
-        await using var provider = TestServices.CreateProvider();
-        var registry = provider.GetRequiredService<DispatcherRegistry>();
-        var properties = typeof(DispatcherRegistry).GetProperties(BindingFlags.Instance | BindingFlags.NonPublic);
-
-        Assert.All(properties, property =>
-            Assert.Equal(typeof(FrozenDictionary<,>), property.PropertyType.GetGenericTypeDefinition()));
     }
 }
